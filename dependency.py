cba2571ecdeebc5ad10d53fd5daebf6d76bee368
Kawaii Selbst <54507994+KawaiiSelbst@users.noreply.github.com>
2023-08-07 07:26:56 +0300

    changes for kaggle

--- dependency.py
+++ dependency.py
@@ -6,13 +6,15 @@ import subprocess
 from pathlib import Path
 from datetime import datetime
 
+
 def install_packages_but_jank_af():
     packages = ['build-essential', 'python3-dev', 'ffmpeg', 'aria2']
     pip_packages = ['pip', 'setuptools', 'wheel', 'httpx==0.23.0', 'faiss-gpu', 'fairseq', 'gradio==3.34.0',
                     'ffmpeg', 'ffmpeg-python', 'praat-parselmouth', 'pyworld', 'numpy==1.23.5',
                     'numba==0.56.4', 'librosa==0.9.2', 'mega.py', 'gdown', 'onnxruntime', 'pyngrok==4.1.12',
                     'gTTS', 'elevenlabs']
-
+    subprocess.run(["apt-get", "update"]) 
+    subprocess.run(["pip", "install --upgrade --force-reinstall scipy==1.9.3 numpy==1.22.4"]) 
     print("Updating and installing system packages...")
     for package in packages:
         print(f"Installing {package}...")
@@ -24,16 +26,27 @@ def install_packages_but_jank_af():
     print('Packages up to date.')
 
 
-def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
-    # Mounting Google Drive
+def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage, Creds, Service):
+    # Mounting 
     if not ForceTemporaryStorage:
-        from google.colab import drive
+        if Service == 'Mega':
+            from rclone.rclone import Rclone
 
-        if not os.path.exists('/content/drive'):
-            drive.mount('/content/drive')
-        else:
-            print('Drive is already mounted. Proceeding...')
-
+            if not 'Mega:' in rc.execute('listremotes'):
+                rc.config(
+                    f"create Mega mega mega user={Creds[0]} pass={Creds[1]} --non-interactive"
+                )       
+            else:
+                print('Remote:Mega is already created. Proceeding...')
+            def rc_exists(file_path):
+                if 'error listing: directory not found' in rc.ls(file_path):
+                    return False
+                else:
+                    for file in rc.ls(file_path):
+                        if file in file_path:
+                            return True
+                    
+            
     # Function to install dependencies with progress
     def install_packages():
         packages = ['build-essential', 'python3-dev', 'ffmpeg', 'aria2']
@@ -41,7 +54,8 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
                         'ffmpeg', 'ffmpeg-python', 'praat-parselmouth', 'pyworld', 'numpy==1.23.5',
                         'numba==0.56.4', 'librosa==0.9.2', 'mega.py', 'gdown', 'onnxruntime', 'pyngrok==4.1.12',
                         'gTTS', 'elevenlabs']
-
+        subprocess.run(["apt-get", "update"]) 
+        subprocess.run(["pip", "install --upgrade --force-reinstall scipy==1.9.3 numpy==1.22.4"]) 
         print("Updating and installing system packages...")
         for package in packages:
             print(f"Installing {package}...")
@@ -95,31 +109,32 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
 
     # Check if CachedRVC.tar.gz exists
     if ForceTemporaryStorage:
-        file_path = '/content/CachedRVC.tar.gz'
+        file_path = '/kaggle/working/CachedRVC.tar.gz'
     else:
-        file_path = '/content/drive/MyDrive/RVC_Cached/CachedRVC.tar.gz'
+        file_path = 'Mega:RVC_Cached/CachedRVC.tar.gz'
 
-    content_file_path = '/content/CachedRVC.tar.gz'
+    content_file_path = '/kaggle/working/CachedRVC.tar.gz'
     extract_path = '/'
 
-    if not os.path.exists(file_path):
+    if not rc_exists(file_path):
         folder_path = os.path.dirname(file_path)
-        os.makedirs(folder_path, exist_ok=True)
+        rc.mkdir(f"{folder_path} --ignore-existing")
         print('No cached dependency install found. Attempting to download GitHub backup..')
 
         try:
             download_url = "https://github.com/kalomaze/QuickMangioFixes/releases/download/release3/CachedRVC.tar.gz"
-            subprocess.run(["wget", "-O", file_path, download_url])
+            subprocess.run(["wget", "-O", "/kaggle/tmp/", download_url])
+            rc.copy('/kaggle/tmp/CachedRVC.tar.gz', file_path)
             print('Download completed successfully!')
         except Exception as e:
             print('Download failed:', str(e))
 
             # Delete the failed download file
             if os.path.exists(file_path):
-                os.remove(file_path)
+                rc.delete(file_path)
             print('Failed download file deleted. Continuing manual backup..')
 
-    if Path(file_path).exists():
+    if rc_exists(file_path):
         if ForceTemporaryStorage:
             print('Finished downloading CachedRVC.tar.gz.')
         else:
@@ -129,7 +144,7 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
         if ForceTemporaryStorage:
             pass
         else:
-            shutil.copy(file_path, content_file_path)
+            rc.copy(file_path, content_file_path)
 
         print('Beginning backup copy operation...')
 
@@ -148,20 +163,19 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
             ForceUpdateDependencies = False
     else:
         print('CachedRVC.tar.gz not found. Proceeding to create an index of all current files...')
-        scan_and_write('/usr/', '/content/usr_files.csv')
+        scan_and_write('/usr/', '/kaggle/working/usr_files.csv')
 
         install_packages()
 
-        scan_and_write('/usr/', '/content/usr_files_new.csv')
-        changed_files = compare_files('/content/usr_files.csv', '/content/usr_files_new.csv')
+        scan_and_write('/usr/', '/kaggle/working/usr_files_new.csv')
+        changed_files = compare_files('/kaggle/working/usr_files.csv', '/kaggle/working/usr_files_new.csv')
 
-        with tarfile.open('/content/CachedRVC.tar.gz', 'w:gz') as new_tar:
+        with tarfile.open('/kaggle/working/CachedRVC.tar.gz', 'w:gz') as new_tar:
             for file in changed_files:
                 new_tar.add(file)
                 print(f'Added to tar: {file}')
-
-        os.makedirs('/content/drive/MyDrive/RVC_Cached', exist_ok=True)
-        shutil.copy('/content/CachedRVC.tar.gz', '/content/drive/MyDrive/RVC_Cached/CachedRVC.tar.gz')
-        print('Updated CachedRVC.tar.gz copied to Google Drive.')
+        rc.mkdir("Mega:RVC_Cached/RVC_Cached --ignore-existing")
+        rc.copy('/kaggle/working/CachedRVC.tar.gz', 'Mega:RVC_Cached/RVC_Cached/CachedRVC.tar.gz')
+        print('Updated CachedRVC.tar.gz copied to Mega.')
         print('Dependencies fully up to date; future runs should be faster.')
 
